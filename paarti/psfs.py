4ebf6bf0d0a6eebed34b418bc8cf4f8d293b6405
Jessica Lu <jlu.astro@yahoo.com>
2021-01-14 16:51:57 -0800

    Implemented PSF_stack.__init__

--- paarti/psfs.py
+++ paarti/psfs.py
@@ -2,14 +2,46 @@ import numpy as np
 import pylab as plt
 
 class PSF_stack(object):
-    def __init__(self):
-        self.psfs = np.array((n_psfs, psf_x_size, psf_y_size), dtype=float)
-        self.pos = np.array((n_psfs, 2), dtype=float)
+    """
+    An object for a stack of PSFs (often a grid). 
+    This is the general object that should be sub-classed for more
+    package specific features or for loading from their files.
+    """
+    def __init__(self, psfs, pos, pixel_scale, wavelength, bandpass, telescope):
+        """
+        Create a PSF stack object for easily carrying around information needed
+        to analyze and visualize a stack of PSFs.
 
-        self.pixel_scale = 1.0
-        self.wavelength = 1.0
-        self.bandpass = 1.0
-        self.telescope = 'Keck1'
+        Inputs
+        ------
+        psfs : numpy array
+            a 3D stack of PSFs with dimensions [N_psfs, psf_Y_size, psf_X_size]
+        pos : numpy array
+            a 2D array with the (y, x) sky positions for each PSF with dimensions
+            [N_psfs, psf_Y_size, psf_X_size]
+        pixel_scale : float
+            The pixel scale of each PSFs in arcsec / pixel.
+        wavelength : float
+            The central wavelength of the PSFs in nm.
+        bandpass : float
+            The wavelength range or bandpass of the PSFs in nm.
+        telescope : string
+            The name of the telescope. Ideally, it would be one from the dictionary
+            in paarti.telescopes.tel_names (e.g. 'Keck1').
+
+        Usage
+        -----
+        """
+        # 3D array with dimensions of [number of PSFs, psf_Y, psf_X]
+        self.psfs = psfs
+
+        # Give the sky positions (grid_Y, grid_X) of each PSF in the stack
+        self.pos = pos
+
+        self.pixel_scale = pixel_scale
+        self.wavelength = wavelength
+        self.bandpass = bandpass
+        self.telescope = telescope
         
         return
 
